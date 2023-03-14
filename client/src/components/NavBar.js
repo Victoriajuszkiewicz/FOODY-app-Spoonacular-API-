e54028770f5b3baf97f2d655517a98e4025cd4d9
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-03-14 13:06:15 +0700

    navBar in progress

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"; //this is a the css file used in react bootstrap libraries
-import { container } from "react-bootstrap";
+import "./NavBar.css";
+import { GiCookingPot } from "react-icons/gi";
 
 const NavBar = () => {
-  return <nav>THIS SECTION SHOUD BE THE NAV BAR</nav>;
+  return (
+    <nav className="navBar">
+      <GiCookingPot />
+      THIS SECTION SHOUD BE THE NAV BAR
+    </nav>
+  );
 };
 
 export default NavBar;
