1fb478518c7fddbcdb0a3deda45fb23075674de3
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-03-14 16:21:41 +0700

    dropdown now working

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,9 +5,58 @@ import { GiCookingPot } from "react-icons/gi";
 
 const NavBar = () => {
   return (
-    <nav className="navBar">
-      <GiCookingPot />
-      THIS SECTION SHOUD BE THE NAV BAR
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand">Queen's app</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDarkDropdown"
+          aria-controls="navbarNavDarkDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <GiCookingPot />
+          {/* <span className="navbar-toggler-icon"></span> this line add the three line icon*/}
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDarkDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              ></a>
+
+              <ul
+                className="dropdown-menu dropdown-menu-dark"
+                aria-labelledby="navbarDarkDropdownMenuLink"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Login
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Favorites
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    About us
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
