657bab48908a8d8111a4a9d19bf8942de7b9e1de
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-03-14 20:48:55 +0700

    restructure init

--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const InputForm = () => {
-  return <div>InputForm</div>;
+  return <div>Test test</div>;
 };
 
 export default InputForm;
--- model/init_db.sql
+++ model/init_db.sql
@@ -1,37 +1,27 @@
--- AUTH DB (registering)--
-
-
 SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS favorites;
-   DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users;
 SET foreign_key_checks = 1;
  
+CREATE TABLE favorites (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    recipe_id INT NOT NULL,
+    recipe_name VARCHAR(100) NOT NULL,
+    recipe_image VARCHAR(100) NOT NULL,
+    recipe_link VARCHAR(100) NOT NULL
+);
 
-
-    CREATE TABLE users (
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     name VARCHAR(30) NOT NULL,
     email VARCHAR(200) NOT NULL UNIQUE,
-    password varchar(200) NOT NULL
-    );
+    password VARCHAR(200) NOT NULL
+);
 
 --just starting data to test--
 INSERT INTO users
-VALUES (1,'Jim','jim.nugget@nuggetlove.com','$2b$12$eFzMWbS9SogNtxkmo3J7a08FQMIOVsF6GGKpTQdgq.W'
-), 
-(2,'Maya','maya.nugget@nuggetlove.com','$2b$12$eFzMWbS9SogNtxkmo3J7a08FQMIOVsF6GGKpTQdgq.E'
-), 
-(3,'Dodo','dodo.nugget@nuggetlove.com','$2b$12$eFzMWbS9SogNtxkmo3J7a08FQMIOVsF6GGKpTQdgq.R'
-);
+VALUES 
+(1,'Jim','jim.nugget@nuggetlove.com','$2b$12$eFzMWbS9SogNtxkmo3J7a08FQMIOVsF6GGKpTQdgq.W'), 
+(2,'Maya','maya.nugget@nuggetlove.com','$2b$12$eFzMWbS9SogNtxkmo3J7a08FQMIOVsF6GGKpTQdgq.E'), 
+(3,'Dodo','dodo.nugget@nuggetlove.com','$2b$12$eFzMWbS9SogNtxkmo3J7a08FQMIOVsF6GGKpTQdgq.R');
 
-
--- Create Tables
---
-
-CREATE TABLE favorites (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-    recipe_id VARCHAR(40) NOT NULL,
-    recipe_name VARCHAR(100) NOT NULL,
-    recipe_image VARCHAR(200) NOT NULL,
-    recipe_link VARCHAR(200) NOT NULL
-);
