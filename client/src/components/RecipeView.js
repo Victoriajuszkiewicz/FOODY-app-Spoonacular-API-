c0d876f0f377df7c5dae1f0ede4487546c309dba
Sol Cifuentes <msolcifuentes@gmail.com>
2023-03-15 17:30:52 +0100

    object of steps created

--- client/src/components/RecipeView.js
+++ client/src/components/RecipeView.js
@@ -1,16 +1,30 @@
 import React from "react";
 
 export default function RecipeView(props) {
-  const { recipe } = props;
-  //  console.log(recipe)
+  const { recipe, recipeInstructions } = props;
+  if (recipeInstructions) {
+    const extractSteps = recipeInstructions[0];
+    const { steps } = extractSteps;
+    let instructions = {};
+    let ingredients = {};
+    for (let objects of steps) {
+      console.log("objects", objects);
+      instructions[objects.number] = objects.step;
+    }
+    console.log(instructions);
+  }
+
   return (
     <div>
       <h3>{recipe.title}</h3>
       <img src={recipe.image} alt={recipe.title} />
-      {/* <h5>Ingredients</h5> */}
+      {/* <p>{recipeInstructions}</p> */}
+      {/* {recipeInstructions.map((step) => (
+        <p>{step}</p>
+      ))} */}
+
       {/* There's more then one property in the missedIngredients, 
       could we do the map instead hardcode way to render them? */}
-      {/* <p>{recipe.missedIngredients[0].name}</p> */}
     </div>
   );
 }
