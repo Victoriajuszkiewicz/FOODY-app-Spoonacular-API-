88c3d3f0ffa5605955b254f63bcefb068a6722d4
Sol Cifuentes <msolcifuentes@gmail.com>
2023-03-15 14:57:23 +0100

    getRecipeInstructions working

--- client/src/components/RecipeView.js
+++ client/src/components/RecipeView.js
@@ -7,11 +7,10 @@ export default function RecipeView(props) {
     <div>
       <h3>{recipe.title}</h3>
       <img src={recipe.image} alt={recipe.title} />
-      <h5>Ingredients</h5>
+      {/* <h5>Ingredients</h5> */}
       {/* There's more then one property in the missedIngredients, 
       could we do the map instead hardcode way to render them? */}
-      <p>All the ingredients</p>
-      {/* {recipe.missedIngredients[0].name} */}
+      {/* <p>{recipe.missedIngredients[0].name}</p> */}
     </div>
   );
 }
--- client/src/components/ResultView.js
+++ client/src/components/ResultView.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
@@ -7,28 +7,32 @@ import RecipeView from "./RecipeView";
 import Button from "react-bootstrap/Button";
 import { Navigate, useNavigate } from "react-router-dom";
 import SearchBar from "../views/Home/SearchBar";
+import { getSteps } from "../helpers/Api";
 
 export default function ResultView(props) {
-  const { allRecipes, setAllRecipes } = props;
+  const { allRecipes, setAllRecipes, showRecipe } = props;
   const navigate = useNavigate();
 
   return (
     <div>
       {/*Resing the SearchBar component: it is displaying in result page */}
-      <div><SearchBar setAllRecipes={setAllRecipes} /></div>
-      
+      <div>
+        <SearchBar setAllRecipes={setAllRecipes} />
+      </div>
+
       <Container>
         <Row xs={1} md={2} className="g-4">
-           {/*{ length: 1 } => it's duplicating colunme*/}
-          {Array.from({ length: 1 }).map((_, idx) => ( 
-            <Col key={idx}> {/*list needs a unic key*/},
-              {/* xs={12} md={8} */},
+          {/*{ length: 1 } => it's duplicating colunme*/}
+          {Array.from({ length: 1 }).map((_, idx) => (
+            <Col key={idx}>
+              {" "}
+              {/*list needs a unic key*/},{/* xs={12} md={8} */},
               {allRecipes.map((recipe) => (
                 <Card
                   key={recipe.id}
                   className="card-recipe"
                   style={{ width: "18rem" }}
-                  onClick={(event) => props.showRecipe(recipe.id)}
+                  onClick={(event) => showRecipe(recipe.id)}
                 >
                   <Card.Img variant="top" src={recipe.image} />
                   <Card.Body>
@@ -41,7 +45,7 @@ export default function ResultView(props) {
         </Row>
         {/* we need to add the onClick={() => navigate("/")} to do another call and get more recipes */}
         <Button variant="primary">See more recipes</Button>
-        {/* <RecipeView /> */} 
+        {/* <RecipeView /> */}
       </Container>
     </div>
   );
