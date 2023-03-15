10843292421f345eacf23053223481c8360fc75b
Kelly Vy Tran <kt.kellytran@gmail.com>
2023-03-15 12:53:37 +0700

    debug init error

--- model/init_db.sql
+++ model/init_db.sql
@@ -18,7 +18,7 @@ CREATE TABLE users (
     password VARCHAR(200) NOT NULL
 );
 
---just starting data to test--
+
 INSERT INTO users
 VALUES 
 (1,'Jim','jim.nugget@nuggetlove.com','$2b$12$eFzMWbS9SogNtxkmo3J7a08FQMIOVsF6GGKpTQdgq.W'), 
