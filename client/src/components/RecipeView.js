ccad5d340d16f0b44fd61ebd88b4566911276e02
Sol Cifuentes <msolcifuentes@gmail.com>
2023-03-15 12:01:21 +0100

    allRecipes state fixed

--- client/src/components/RecipeView.js
+++ client/src/components/RecipeView.js
@@ -2,16 +2,15 @@ import React from "react";
 
 export default function RecipeView(props) {
   const { recipe } = props;
-//  console.log(recipe)
+  //  console.log(recipe)
   return (
-  <div>
+    <div>
       <h3>{recipe.title}</h3>
-      <img src={recipe.image} />
+      <img src={recipe.image} alt={recipe.title} />
       <h5>Ingredients</h5>
       {/* There's more then one property in the missedIngredients, 
       could we do the map instead hardcode way to render them? */}
-      <p>{recipe.missedIngredients[0].name}</p> 
-
-  </div>
+      <p>{recipe.missedIngredients[0].name}</p>
+    </div>
   );
 }
