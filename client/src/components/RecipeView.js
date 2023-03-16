291fe45960150994af29ad8fa3b5677d11d4760d
Sol Cifuentes <msolcifuentes@gmail.com>
2023-03-16 13:10:31 +0100

    delete unused code

--- client/src/components/RecipeView.js
+++ client/src/components/RecipeView.js
@@ -2,33 +2,19 @@ import React from "react";
 
 export default function RecipeView(props) {
   const { recipe, recipeInstructions, ingredientList } = props;
-  console.log(recipeInstructions);
-
-  // if (recipeInstructions) {
-  //   const recipeExtractor = recipeInstructions[0];
-  //   const { steps } = recipeExtractor;
-  //   let instructions = {};
-  //   for (let objects of steps) {
-  //     instructions[objects.number] = objects.step;
-  //   }
-  //   console.log(instructions);
-  // }
 
   const recipeSteps = [];
-
-  for (const step of recipeInstructions[0].steps) {
-    recipeSteps[step.number] = step.step;
+  if (recipeInstructions) {
+    for (let step of recipeInstructions[0].steps) {
+      recipeSteps[step.number] = step.step;
+    }
   }
-
-  console.log(recipeSteps);
 
   return (
     <div>
       <h3>{recipe.title}</h3>
       <img src={recipe.image} alt={recipe.title} />
 
-      {/* There's more then one property in the missedIngredients, 
-      could we do the map instead hardcode way to render them? */}
       <h3>Ingredient List</h3>
       {ingredientList &&
         ingredientList.map((ingredient, index) => {
