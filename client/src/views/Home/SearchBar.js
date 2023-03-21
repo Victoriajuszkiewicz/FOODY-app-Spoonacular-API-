57d4f80dfe357046c9f14c9cc72663fcee3377dd
Wenyan Lin <wennyan.lin@gmail.com>
2023-03-21 11:28:20 +0100

    Fix typo local.js import path

--- client/src/views/Home/SearchBar.js
+++ client/src/views/Home/SearchBar.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import Select from "react-select";
 import { useNavigate } from "react-router-dom";
-import ResultView from "../../components/ResultView";
 import { Api } from "../../helpers/Api";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "./SearchBar.css";
