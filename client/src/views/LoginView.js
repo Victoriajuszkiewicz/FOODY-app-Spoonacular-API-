08b0c198b7fb3ec336777af5fb5d48c2e9c8bab1
Victoriajuszkiewicz <wanderer.foxy@gmail.com>
2023-04-06 16:30:24 +0200

    cleaner code

--- client/src/views/LoginView.js
+++ client/src/views/LoginView.js
@@ -26,9 +26,7 @@ const LoginView = (props) => {
   const handleSubmit = (event) => {
     event.preventDefault();
     inputLoginCb(loginInput);
-
     setLoginInput(INIT_LOGINFORM);
-    // console.log("someone want to log in... oh lala");
   };
 
   //TOGGLE BUTTON
@@ -206,109 +204,3 @@ const LoginView = (props) => {
 };
 
 export default LoginView;
-
-// import { useState } from "react";
-// import InputBox from "../components/InputBox";
-// import { useNavigate } from "react-router-dom";
-// import {
-//   Container,
-//   Form,
-//   Button,
-//   Nav,
-//   ToggleButton,
-//   ButtonGroup,
-// } from "react-bootstrap";
-// import "./LoginView.css";
-
-// const INIT_LOGINFORM = {
-//   emailLogin: "",
-//   passwordLogin: "",
-// };
-
-// const LoginView = (props) => {
-//   const [loginInput, setLoginInput] = useState(INIT_LOGINFORM);
-//   const navigate = useNavigate();
-//   //TOGGLE BUTTON
-//   // const [active, isActive] = useState(false);
-//   // const [buttonValue, setButtonValue] = useState("1");
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setLoginInput({ ...loginInput, [name]: value });
-//   };
-
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     props.inputLoginCb(loginInput);
-//     setLoginInput(INIT_LOGINFORM);
-//     // console.log("someone want to log in... oh lala");
-//   };
-
-//   //TOGGLE BUTTON
-//   const buttons = [
-//     { name: "Register", value: "1" },
-//     { name: "Login", value: "2" },
-//   ];
-
-//   return (
-//     <Container>
-//       <Form onSubmit={handleSubmit} className="form-container">
-
-//         <h2>Welcome back</h2>
-
-//         <Form.Group className="d-flex flex-column align-items-center">
-//           <Form.Label style={{ marginBottom: "0.2rem" }}>
-//             Email Address
-//           </Form.Label>
-//           <Form.Control
-//             style={{ width: "18rem" }}
-//             id="email"
-//             type="text"
-//             placeholder="nugget@example.com"
-//             name="emailLogin"
-//             value={loginInput.emailLogin}
-//             label="Email"
-//             onChange={handleChange}
-//             required
-//           />
-//           <Form.Label style={{ marginBottom: "0.2rem" }}>Password</Form.Label>
-//           <Form.Control
-//             style={{ width: "18rem" }}
-//             id="password"
-//             type="password"
-//             name="passwordLogin"
-//             placeholder="your password..."
-//             value={loginInput.passwordLogin}
-//             label="Password"
-//             onChange={handleChange}
-//             required
-//           />
-//         </Form.Group>
-//         <div>
-//           <Button
-//             style={{ marginTop: "15px" }}
-//             variant="secondary"
-//             type="submit"
-//           >
-//             Log in
-//           </Button>
-
-//           <div>
-//             <p className="register-btn-container">
-//               If you don't have an account, click{" "}
-//               <button
-//                 className="button-here"
-//                 onClick={(m) => navigate("/register")}
-//               >
-//                 here
-//               </button>
-//             </p>
-//           </div>
-
-//         </div>
-//       </Form>
-//     </Container>
-//   );
-// };
-
-// export default LoginView;
